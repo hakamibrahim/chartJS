4eb8d1ac14b8b7eb34537862c12f7ccf8cd97ae1
h4km <hakamibrahim7@gmail.com>
2018-03-30 21:56:54 +0700

    first commit

---
+++ data.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Content-Type: application/json');
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'chartjs');
+
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+    die('Connection failed: ' . $mysqli->error);
+}
+
+$query = sprintf("SELECT playerid, score FROM score ORDER BY playerid");
+
+$result = $mysqli->query($query);
+
+$data = array();
+foreach ($result as $row){
+    $data[] = $row;
+}
+
+$result->close();
+$mysqli->close();
+
+print json_encode($data);
---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "http://localhost/chart/grafikChartJS/data.php",
+        method: "GET",
+        success: function(data){
+            console.log(data);
+            var pemain = [];
+            var score = [];
+
+            for(var i in data){
+                pemain.push("Player " + data[i].playerid);
+                score.push(data[i].score);
+            }
+
+            var chartdata = {
+                labels: pemain,
+                datasets : [
+                    {
+                        label: 'Player Score',
+                        backgroundColor: 'rgba(200, 200, 200, 0.75)',
+                        borderColor: 'rgba(200, 200, 200, 0.75)',
+                        hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                        hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                        data: score
+                    }
+                ]
+            };
+
+            var ctx = $("#mycanvas");
+            var barGraph = new Chart(ctx, {
+                type : 'bar',
+                data: chartdata
+            });
+        },
+        error: function(data){
+            console.log(data);
+        }
+    });
+});
